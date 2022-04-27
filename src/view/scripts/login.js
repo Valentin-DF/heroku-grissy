f54633ecbff5accca4a0e8ca9a033582936e6e2d
Valentin-DF <83488747+Valentin-DF@users.noreply.github.com>
2022-04-27 15:29:47 -0500

    mejoramiento del login

--- src/view/scripts/login.js
+++ src/view/scripts/login.js
@@ -12,17 +12,43 @@ var login = function () {
                     contrasena: contrasena
                 },
                 success: function (response) {
-                    console.log(response);
-                    var objListado = JSON.parse(response);
-                    $(objListado).each(function (i, obj) {
-                        if(obj.estado == 1){                       
-                             localStorage.setItem("empleado",JSON.stringify( obj));
-                             window.location.replace('http://localhost:8080/Grissy/src/view/menu.html');
 
-                        }else{
-                            console.log("Erorosssssssssr");
-                        }
-                    });
+                    console.log(response)
+                    var object = [];
+                    var objListado = JSON.parse(response);
+                    if (objListado != "") {
+                        $(objListado).each(function (i, obj) {
+                    
+                            if (obj.estado == 1) {
+                                localStorage.setItem("empleado", JSON.stringify(obj));
+                                window.location.replace('http://localhost:8080/Grissy/src/view/menu.html');
+                                console.log(response);
+                    
+                                Toastify({
+                                    text: "Su cuenta se encuentra activa",
+                                    duration: 3000,
+                                    close: true,
+                                    backgroundColor: "#171ae3",
+                                }).showToast();
+                            } else {
+                                Toastify({
+                                    text: "Su cuenta se encuentra inactiva",
+                                    duration: 3000,
+                                    close: true,
+                                    backgroundColor: "#be4f60",
+                                }).showToast();
+                            }
+                        });
+                    } else {
+                        Toastify({
+                            text: "Usted no cuenta con cuenta",
+                            duration: 3000,
+                            close: true,
+                            backgroundColor: "#be4f60",
+                        }).showToast();
+                    }
+                    
+
                 }
             })
 
