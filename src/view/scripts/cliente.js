cd2cee94f27e21a8a8682dc20562ca5a274f6b0a
Valentin-DF <83488747+Valentin-DF@users.noreply.github.com>
2022-05-04 10:58:34 -0500

    pdf, excel, imprimir

--- src/view/scripts/cliente.js
+++ src/view/scripts/cliente.js
@@ -61,6 +61,27 @@ var cliente = function () {
                 "language": {
                     "url": "https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json"
                 },
+                "dom": "Bfrtilp",
+                "buttons":[
+                    {
+                        "extend":"excelHtml5",
+                        "text":"<i class='fas fa-file-excel'></i>",
+                        "titleAttr":"Exportar a Excel",
+                        "className":"btn btn-success"
+                    },
+                    {
+                        "extend":"pdfHtml5",
+                        "text":"<i class='fas fa-file-pdf'></i>",
+                        "titleAttr":"Exportar a PDF",
+                        "className":"btn btn-danger"
+                    },
+                    {
+                        "extend":"print",
+                        "text":"<i class='fa fa-print'></i>",
+                        "titleAttr":"Imprimir",
+                        "className":"btn btn-info"
+                    }
+                ]
 
             });
             cliente.obtener_data_editar("#example tbody", table);
--- src/view/scripts/producto.js
+++ src/view/scripts/producto.js
@@ -22,6 +22,27 @@ var producto = function () {
                 "language": {
                     "url": "https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json"
                 },
+                "dom": "Bfrtilp",
+                "buttons":[
+                    {
+                        "extend":"excelHtml5",
+                        "text":"<i class='fas fa-file-excel'></i>",
+                        "titleAttr":"Exportar a Excel",
+                        "className":"btn btn-success"
+                    },
+                    {
+                        "extend":"pdfHtml5",
+                        "text":"<i class='fas fa-file-pdf'></i>",
+                        "titleAttr":"Exportar a PDF",
+                        "className":"btn btn-danger"
+                    },
+                    {
+                        "extend":"print",
+                        "text":"<i class='fa fa-print'></i>",
+                        "titleAttr":"Imprimir",
+                        "className":"btn btn-info"
+                    }
+                ]
 
             });
             this.obtener_data_editar("#example tbody", table);
